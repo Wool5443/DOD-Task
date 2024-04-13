493e0184508b9e6081183db91bbbf0bce59a4914
Wool5443 <mihsolodilov2015@gmail.com>
2024-04-14 00:28:58 +0300

    Solution

--- main.cpp
+++ main.cpp
@@ -8,10 +8,15 @@ void DrawMan(int x, int y);
 int main()
 {
     txCreateWindow(WIDTH, HEIGHT);
-    txSetFillColor(TX_WHITE);
-    txFloodFill(0, 0);
 
-    DrawMan(300, 300);
+    for (int i = 0; i < 50; i++)
+    {
+        txSetFillColor(TX_WHITE);
+        txRectangle(0, 0, WIDTH, HEIGHT);
+
+        DrawMan(100 + i * 10, 500);
+        txSleep(50);
+    }
 
     return 0;
 }
@@ -21,13 +26,13 @@ void DrawMan(int x, int y)
     txSetColor    (TX_BLACK);
     txSetFillColor(TX_BLACK);
 
-    txLine(385, 450 - 42, 385, 450 - 84);
+    txLine(x, y - 42, x, y - 84);
 
-    txLine(385, 450 - 42, 385 - 30, 450);
-    txLine(385, 450 - 42, 385 + 30, 450);
+    txLine(x, y - 42, x - 30, y);
+    txLine(x, y - 42, x + 30, y);
 
-    txLine(385, 450 - 78, 385 - 30, 450 - 48);
-    txLine(385, 450 - 78, 385 + 50, 450 - 84);
+    txLine(x, y - 78, x - 30, y - 48);
+    txLine(x, y - 78, x + 50, y - 84);
 
-    txCircle(385, 450 - 120 + 18, 18);
+    txCircle(x, y - 120 + 18, 18);
 }
